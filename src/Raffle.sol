7e399e83cecfc488736f68af75c8fb984d11f4d7
Hebx <lord.noctx@gmail.com>
2024-01-03 01:54:17 +0100

    Forge Init

---
+++ src/Raffle.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+/**
+ * @title Raffle Lottery contract
+ * @author Ihab Heb
+ * @notice This contract is for creating a raffle lottery
+ * @dev Implements Chainlink VRFv2
+ */
+contract Raffle {
+
+}
